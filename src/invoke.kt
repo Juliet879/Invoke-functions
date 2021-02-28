b19750d8a8e8a6b9d1cb46153f3e1c4b03fd6b2b
juliet879 <gisembajuliett@gmail.com>
2021-02-28 07:33:11 +0300

    add modulus function

--- src/invoke.kt
+++ src/invoke.kt
@@ -1,11 +1,16 @@
 fun main() {
     name()
+    var answer = modulus(40,3)
+    println(answer)
 }
+
 fun name() {
     var name = "Juliet"
     println("Hello" +" " + name)
 }
-fun remainder() {
-    var num1 = 40
-    var num2 = 3
+
+fun modulus(num1:Int,num2:Int):Int {
+    var remainder = num1 % num2
+
+    return remainder
 }
